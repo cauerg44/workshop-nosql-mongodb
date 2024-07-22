84f091129e5a75b1c8f9d891a851aca0d7f6b35c
Caue Garcia <cauerg7@outlook.com>
2024-07-22 15:43:19 -0300

    GET Post by title, query operator

--- src/main/java/com/devsuperior/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/workshopmongodb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.devsuperior.workshopmongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.workshopmongodb.models.entities.Post;
@@ -10,5 +11,8 @@ import com.devsuperior.workshopmongodb.models.entities.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
